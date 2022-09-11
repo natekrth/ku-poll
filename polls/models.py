7594af46a570a7aee8f21e0b6eb40cb9efa1ad1e
Zion Keretho <natekrth@gmail.com>
2022-09-12 00:34:12 +0700

    write more condition

--- polls/models.py
+++ polls/models.py
@@ -7,22 +7,27 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('date ended', default=None, null=True)
     
     def was_published_recently(self):
+        """Questoin was published less than or equal to 1 day."""
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
     def is_published(self):
+        """Check that Question is published at this time."""
         now = timezone.localtime().now()
         return now >= self.pub_date
     
     def can_vote(self):
+        """Check that Question can be vote."""
         now = timezone.localtime().now()
-        if self.end_date is None:
+        if self.end_date is None and self.pub_date <= now:
             return True
         return self.pub_date <= now <= self.end_date
-    
+        
     def __str__(self):
+        """Representative of Question object."""
         return self.question_text
 
 
