236df12fe846ea1a1851d51425d4ca2ee408c3c6
Zion Keretho <natekrth@gmail.com>
2022-09-19 11:28:31 +0700

    remove import choice

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Choice, Question, Choice, Vote
+from .models import Question, Choice, Vote
 
 admin.site.register(Question)
 admin.site.register(Choice)
